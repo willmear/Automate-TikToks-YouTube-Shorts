3e556651bf12cb0f314eff85cb2418b9982fa6ce
Will Mear <107112336+willmear@users.noreply.github.com>
2024-05-24 13:18:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import tts
+
+def main():
+    tts.tts("c1221a037e2895f84567c57959d7b4b4")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ tts.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from openai import OpenAI
+import os
+client = OpenAI(
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+
+def tts():
+  speech_file_path = Path(__file__).parent / "speech.mp3"
+  response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input="Today is a wonderful day to build something people love!"
+  )
+
+  response.stream_to_file(speech_file_path)
